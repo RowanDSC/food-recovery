e8fa5b27307774fc1d1199d32c5da1af0f46a301
Evan Pierce <mrthinger@gmail.com>
2020-08-18 00:12:18 -0400

    create common

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import {commonHello} from "@dsc/common";
 
 const PORT = process.env.PORT ?? 5000
 const app = express()
  
 app.get('/', function (req, res) {
+  commonHello();
   res.send('Hello World')
 })
  
