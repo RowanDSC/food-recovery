d370f45fb80cac8b3054f0df788459ceb19194f6
Evan Pierce <mrthinger@gmail.com>
2020-08-18 20:49:11 -0400

    adjust ts build

--- packages/common/lib/common.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.commonHello = void 0;
-exports.commonHello = () => {
-    console.log('Hello from common!');
-};
-//# sourceMappingURL=common.js.map
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,14 +1,13 @@
 import express from "express";
-import {commonHello} from "@dsc/common";
+import { commonHello } from "@dsc/common";
 
-const PORT = process.env.PORT ?? 5000
-const app = express()
- 
-app.get('/', function (req, res) {
-  commonHello();
-  res.send('Hello World')
-})
- 
+const PORT = process.env.PORT ?? 5000;
+const app = express();
+
+app.get("/", function (req, res) {
+  res.send(commonHello());
+});
+
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
 );
