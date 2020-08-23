b34006d9a8c0f5ce44e35201e45809747cb33f01
Evan Pierce <evanpierce@aim.com>
2020-08-23 13:42:35 -0400

    comment for clarification

--- packages/frontend/__test__/index.test.ts
+++ packages/frontend/__test__/index.test.ts
@@ -1,4 +1,4 @@
-import * as placeholder from "jest";
+import * as placeholder from "jest"; //placeholder workaround for isolatedModules flag
 
 describe("placeholder", () => {
   test("placeholder test", () => {});
